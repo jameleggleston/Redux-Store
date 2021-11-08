de5a518dcfa514831044cf680ca8ff8d1ca20f06
God of War <jamel.eggleston@gmail.com>
2021-11-08 13:43:32 -0500

    imported useSelector and useDispatch

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,9 +6,11 @@ import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 import spinner from '../../assets/spinner.gif';
+import { useSelector, useDispatch } from 'react-redux';
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
 
   const { currentCategory } = state;
 
